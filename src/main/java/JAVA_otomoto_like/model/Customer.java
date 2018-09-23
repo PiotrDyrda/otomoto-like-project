4f6aca002960f7200ab77cc43987aab84e92dbac
Piotr <dyrdapiotr@wp.pl>
2018-09-23 10:57:17 +0200

    customer class model

--- src/main/java/JAVA_otomoto_like/model/Customer.java
+++ src/main/java/JAVA_otomoto_like/model/Customer.java
@@ -14,4 +14,71 @@ public class Customer {
     @Column
     private String name;
 
+    @Column
+    private String surname;
+
+    @Column
+    private String address;
+
+    @Column
+    private Integer nipNumber;
+
+    @Column
+    private Integer idNumber;
+
+    public Customer(String name, String surname, String address, Integer nipNumber, Integer idNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.nipNumber = nipNumber;
+        this.idNumber = idNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getNipNumber() {
+        return nipNumber;
+    }
+
+    public void setNipNumber(Integer nipNumber) {
+        this.nipNumber = nipNumber;
+    }
+
+    public Integer getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(Integer idNumber) {
+        this.idNumber = idNumber;
+    }
 }
