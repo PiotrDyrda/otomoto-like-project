339eceb8e57f884630ea3dde261c5890f43938c7
Piotr <dyrdapiotr@wp.pl>
2018-09-23 11:00:33 +0200

    employee class model

---
+++ src/main/java/JAVA_otomoto_like/model/Employee.java
@@ -0,0 +1,74 @@
+package JAVA_otomoto_like.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private String address;
+
+    @Column
+    private Date employementDate;
+
+
+    public Employee(String name, String surname, String address, Date employementDate) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.employementDate = employementDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getEmployementDate() {
+        return employementDate;
+    }
+
+    public void setEmployementDate(Date employementDate) {
+        this.employementDate = employementDate;
+    }
+}
