bc2c483a1a90b0ed90f3974321193fa2746e48f3
Piotr <dyrdapiotr@wp.pl>
2018-09-23 09:41:02 +0200

    setting_up

---
+++ src/main/java/JAVA/PROJEKT/KONCOWY/OtomotoLikeApplication.java
@@ -0,0 +1,12 @@
+package JAVA.PROJEKT.KONCOWY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtomotoLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtomotoLikeApplication.class, args);
+	}
+}
